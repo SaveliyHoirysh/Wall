cb9c4cf8a1bafcfb47b77288cb1fcdaba0885f1a
SaveliyHoirysh <98319435+SaveliyHoirysh@users.noreply.github.com>
2023-05-20 12:15:55 +0500

    Wallet

---
+++ script.js
@@ -0,0 +1,32 @@
+let purcashe = {};
+
+
+
+let inputName = document.querySelector("#tovar")
+let inputPrice = document.querySelector("#zena")
+let inputCategory = document.querySelector("#selectCategory")
+let btn = document.querySelector(".button");
+purcashe.name = form.name.value;
+purcashe.price = form.price.value;
+purcashe.category = form;
+
+
+
+
+
+btn.insertAdjacentHTML('beforeEnd',
+`
+<tr class="purcashes__item purcashes__row" data-category="product">
+    <td class="purscashes__td">${}</td>
+    <td class="purscashes__td">${}</td>
+    <td class="purscashes__td">${}</td>
+    <td class="purscashes__td"><i class="purshes__item-del fa-solid fa-xmark"></i></td>
+</tr>
+
+
+
+
+
+`
+)
+
